ba4480ed4df648d29532fca16b8084c28efc9856
BABAR1532 <ankitkumar6262@outlook.com>
2022-08-20 13:05:16 +0530

    Marks question solved.

--- Main.java
+++ Main.java
@@ -235,8 +235,79 @@ Scanner sc = new Scanner (System.in);
      } 
        */
 
+     /* Write a java program to input marks of five subjects Physics, Chemistry, Biology, Mathematics and
+       Computer. Calculate percentage and grade according to following:
+           Percentage >= 90% : Grade A
+           Percentage >= 80% : Grade B
+           Percentage >= 70% : Grade C
+           Percentage >= 60% : Grade D
+           Percentage >= 40% : Grade E
+           Percentage < 40% : Grade F     */
+      
+    /*   System.out.println("Hi! Enter your number in-");
+
+       System.out.print("Physics:");
+
+        float physics = sc.nextFloat();
+
+       System.out.print("Chemistry:");
+
+        float chemistry = sc.nextFloat();
+
+       System.out.print("Biology:");
+
+        float biology = sc.nextFloat();
+     
+       System.out.print("Math:");
+
+        float math = sc.nextFloat();
+
+       System.out.print("Computer:");
+
+        float computer = sc.nextFloat();
+
+      float totalmarks = physics + chemistry + biology + math + computer;
+
+      float percentage = (totalmarks / 500 ) * 100;
+
+      if (percentage >= 90){
+
+       System.out.println("Congrats you achieved " + percentage + "%.\n" + "You get a grade A." );
+
+      }
+
+      else if (percentage >= 80){
+
+        System.out.println("Congrats you achieved " + percentage + "%.\n" + "You get a grade B." );
+      }
+
+      else if(percentage >= 70){
+
+        System.out.println("Congrats you achieved " + percentage + "%.\n" + "You get a grade C." );
+      }
+
+      else if(percentage >= 60){
+
+        System.out.println("Congrats you achieved " + percentage + "%.\n" + "You get a grade D." );
+      }
+
+      else if(percentage >= 40){
+
+        System.out.println("Congrats you achieved " + percentage + "%.\n" + "You get a grade E." );
 
 
+      }
+
+      else if(percentage < 40 ){
+           
+        System.out.println("Congrats you achieved " + percentage + "%.\n" + "You get a grade F." );
+      }
+
+      else{
+
+        System.out.println("invalid input!");
+      }
+        */
 
 
 
