54cc7c529faa928620a02b2d1df641ec41ef5eb7
BABAR1532 <ankitkumar6262@outlook.com>
2022-08-20 17:06:16 +0530

    Salary and electricity bill question solved.

--- Main.java
+++ Main.java
@@ -309,23 +309,101 @@ Scanner sc = new Scanner (System.in);
       }
         */
 
+   /* Write a java program to input basic salary of an employee and calculate its Gross salary according to following:
+      Basic Salary <= 10000 : HRA = 20%, DA = 80%
+      Basic Salary <= 20000 : HRA = 25%, DA = 90%
+      Basic Salary > 20000 : HRA = 30%, DA = 95%  */ 
+
+    /*   System.out.println("Enter your Basic salary:");
+
+      double basic = sc.nextDouble();
+        
+       double gross;
+      double da = 0;
+      double hra = 0;
+      
+
+      if (basic <= 10000){
+ 
+         da = basic * 0.80;
+         hra = basic * 0.20;
+      
+      } 
+
+      else if(basic <= 20000){
+
+
+         da = basic * 0.90;
+         hra = basic * 0.25; 
+
+        
+      }
+
+      else if(basic > 20000){
+
+           da = basic * 0.95;
+           hra = basic * 0.30;
+
+        
+      }   
+
+      else{
+
+        System.out.println("Invalid input!");
+      } 
+
+
+      gross = basic + hra + da ;
+
+      System.out.println("Your gross salary is " + gross); */ 
 
 
 
+ /* Write a java program to input electricity unit charges and calculate total electricity bill according to the
+     given condition:
+   for first 50 units Rs. 0.50/unit
+   for next 100 units Rs. 0.75/unit
+   for next 100 units Rs. 1.20/unit
+   for unit above 250 Rs. 1.50/unit
+  An additional surcharge of 20% is added to the bill */
 
 
+ /* System.out.println("Enter the eletricity bill unit: ");
 
+   int unit = sc.nextInt();
+   double bill = 0;
+   double extra;
 
+   if(unit <= 50){
 
+    bill = unit * 0.50 ;
+   } 
 
+   else if(unit <= 150){
 
+    bill = 25 + ((unit - 50) * 0.75);
 
+   } 
 
+   else if(unit <= 250){
 
+    bill = 100 + ((unit - 150) * 1.20);
+   }
 
+   else if(unit > 250){
 
+     bill = 300 + ((unit - 250) * 1.50);
+     
+   }
+  
+   extra = bill * 0.20;
+   bill += extra; 
+  
+  System.out.println("Total eletricity bill is Rs " + bill);
+                                                             */
 
+}
 }
 
 
-}
+
