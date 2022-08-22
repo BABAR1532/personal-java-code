25fae3c14e2861327284275e112331fd27d9f602
BABAR1532 <ankitkumar6262@outlook.com>
2022-08-22 13:17:23 +0530

    switch week day solved.

---
+++ Switch.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Switch{
+
+ public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+
+// Write a java program to print day of week name using switch case.
+
+/* System.out.println("Enter the day number:");
+
+ int day = sc.nextInt();
+
+switch(day){
+
+ case 1 :
+           System.out.println("Its sunday!");
+            break;
+ case 2 :
+           System.out.println("Its monday!");
+           break;
+ case 3 :
+           System.out.println("Its tuesday!");
+           break;
+ case 4 :
+           System.out.println("Its wednesday!");
+           break;
+ case 5 :
+           System.out.println("Its thrusday!");
+           break;
+ case 6 :
+           System.out.println("Its friday!");
+           break;
+ case 7 :
+           System.out.println("Its saturday!");
+           break;
+ default:
+           System.out.println("Please enter valid input!");
+                                                                  */
+
+
+}
+
+
+
+ 
+
+}
