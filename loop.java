5d26338acc854975e44f8947cc88752a50c55bcc
BABAR1532 <ankitkumar6262@outlook.com>
2022-08-23 13:29:06 +0530

    sum of first n number and table of n is completed.

---
+++ loop.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class loop{
+   
+   public static void main(String[] args){
+     
+     Scanner sc = new Scanner(System.in);
+
+    
+// Write a program to print numbers from 1 to 10.
+
+  /*   int num = 1;
+
+     while ( num <= 10){
+
+     	System.out.println(num);
+
+     	num++;
+     } */
+
+// Write a program to calculate the sum of first 10 natural number.     
+     
+   /*  int number = 1;
+      int sum = 0;
+
+     while(number <= 10){
+
+      sum += number;
+
+      number ++;
+
+     }
+
+     System.out.println("Sum of first 10 natural number is " + sum); */
+
+// Write a program that prompts the user to input a positive integer.It should then print the multiplication table of that number.
+
+    /*  System.out.println("Enter your (+) number for multiplication table:");
+
+      int num1 = sc.nextInt();      
+
+      int product;
+
+      for (int num2 = 1; num2 <= 10; num2++){
+
+          product = num1 * num2;
+
+          System.out.println(num1 + " X " + num2 + " = " + product);
+
+      } */
+
+
+
+ }
+}
