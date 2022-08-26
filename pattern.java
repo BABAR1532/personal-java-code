d0ca15563e0cacb777b3886d92340adc8e15b510
BABAR1532 <ankitkumar6262@outlook.com>
2022-08-26 10:34:57 +0530

    first pattern question solved(square pattern solved).

---
+++ pattern.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+ 
+public class pattern{
+
+	public static void main(String[] args){
+    
+    Scanner sc = new Scanner(System.in);
+
+/* 1. Square pattern in java.
+
+      ******                // summary: Easy question, the funda is you have to run inner loop and  
+      ******                            outer loop according to  no of rows because,no of ro- 
+      ******                            ws and column is same and just print star in inner loop.
+      ******
+      ****** */
+
+ /*   int number;
+    System.out.println("Enter no of line you want in square pattern:");
+    number = sc.nextInt();
+   
+    for(int i = 1; i <= number; i++)
+    {
+    	for(int col = 1; col <= number;col++)           
+        {                                              
+        	System.out.print("* ");
+        }
+
+      System.out.println(); 
+
+    }   */                       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
+
