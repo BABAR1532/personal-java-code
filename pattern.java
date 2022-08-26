f441244a910d24604fed2aa3fada6a5ff9908477
BABAR1532 <ankitkumar6262@outlook.com>
2022-08-26 13:04:57 +0530

    Square pattern with number solved.

--- pattern.java
+++ pattern.java
@@ -14,7 +14,7 @@ public class pattern{
       ******
       ****** */
 
- /*   int number;
+ /* int number;
     System.out.println("Enter no of line you want in square pattern:");
     number = sc.nextInt();
    
@@ -27,9 +27,28 @@ public class pattern{
 
       System.out.println(); 
 
-    }   */                       
+    }  */                       
 
+/* Print this pattern question.
+      1 1 1
+      2 2 2                       //summary: Same as pervious question,just change the star and
+      3 3 3                                  with row no i.e print the value of i in inner loop.
+      4 4 4   */
 
+ 
+ /* int number;
+    System.out.println("Enter the no of rows your want for number square.");
+    number = sc.nextInt();
+  
+   for(int i = 1; i <= number; i++)
+   {
+       for(int col = 1; col<= number; col++)
+       {
+           System.out.print(i + " ");
+       }
+
+      System.out.println();  
+   } */  
 
 
 
